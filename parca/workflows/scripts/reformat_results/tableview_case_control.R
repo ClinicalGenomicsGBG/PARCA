336ed65c9dfee22a788da118a579ec70f13acbcb
Pernilla Ericsson <pernilla.ericsson@gu.se>
2021-01-14 11:25:35 +0100

    Replace na with 0

--- parca/workflows/scripts/reformat_results/tableview_case_control.R
+++ parca/workflows/scripts/reformat_results/tableview_case_control.R
@@ -13,13 +13,13 @@ tableview_case_control_out  <- snakemake@output[['case_control']]
 
 
 df_case <- data.table::fread(input = tableview_case, fill = TRUE, sep="\t", header=TRUE) %>% 
-  as_tibble()
+  as_tibble() %>% mutate(tax_id=as.character(tax_id))
 
 df_control <- data.table::fread(input = tableview_control, fill = TRUE, sep="\t", header=TRUE) %>% 
-  as_tibble()
+  as_tibble() %>% mutate(tax_id=as.character(tax_id))
 
 df_case %>% full_join(df_control,
                       by=c("superkingdom", "organism", "tax_id"),
                       suffix = c("_case","_control")) %>% 
-  mutate(across(!matches(c("superkingdom", "organism", "tax_id")), ~replace_na(., 0) )) %>% 
+  mutate(across(where(is.numeric), ~replace_na(., 0) )) %>% 
   write_tsv(tableview_case_control_out, col_names = TRUE)
