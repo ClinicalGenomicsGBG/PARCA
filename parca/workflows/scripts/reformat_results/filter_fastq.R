288189270ad19c4e1be71afbaf31689b63edab08
Pernilla Ericsson <pernilla.ericsson@gu.se>
2021-01-17 17:46:21 +0100

    fixed regex to check for whole world so that it not only works for PE

--- parca/workflows/scripts/reformat_results/filter_fastq.R
+++ parca/workflows/scripts/reformat_results/filter_fastq.R
@@ -13,7 +13,10 @@ suppressPackageStartupMessages({
 # SE_or_PE <- "PE"
 # negate_query <- "FALSE"
 # 
-# fastq_out <- "/Users/pernillaericsson/Desktop/parca_tests/taxid.fastq"
+# fastq_out <- "/Users/pernillaericsson/Desktop/parca_tests/taxid_test.fastq"
+
+trimmed_reads <- "/Users/pernillaericsson/Documents/medair1/medstore/logs/pipeline_logfiles/parca/20201202_run_1_v2_all/snakemake_results_sample_2/SE_DNA/stage1/trimming/trimmed_reads.fq"
+organism_tableview <- "/Users/pernillaericsson/Documents/medair1/medstore/logs/pipeline_logfiles/parca/20201202_run_1_v2_all/snakemake_results_sample_2/SE_DNA/stage8/tableview/organism_dir/organism_100272.tsv"
 
 
 organism_tableview <- snakemake@input[['organism_tableview']]
@@ -40,7 +43,7 @@ if (SE_or_PE == "SE") {
 append_to_fastq <- function(fastq_path, ids, outputFile, negate_choice) {
   # Function to append reads to a fastq file.
   fastq <- ShortRead::readFastq(fastq_path)
-  i_query <- ids %>% map_chr(~{glue::glue("^{.x}\\s")}) %>% paste(collapse = "|")
+  i_query <- ids %>% map_chr(~{glue::glue("\\b{.x}\\b")}) %>% paste(collapse = "|")
   subset <- fastq[ str_detect(ShortRead::id(fastq), i_query, negate = negate_choice) ]
   ShortRead::writeFastq(object = subset, file = outputFile, mode="a", compress = FALSE)
 }
